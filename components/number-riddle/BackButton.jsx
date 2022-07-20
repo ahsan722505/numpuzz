83de353b25e1395d80d3e0d99ccf3d50eead39a6
ahsan722505 <ahsantahir722505@gmail.com>
2022-07-20 21:59:49 +0500

    back

--- components/number-riddle/BackButton.jsx
+++ components/number-riddle/BackButton.jsx
@@ -4,12 +4,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 import { useContext } from 'react';
 import GameContext from "../../store/number-riddle/GameContext";
-const BackButton = () => {
+import { useRouter } from 'next/router';
+const BackButton = ({home}) => {
     const {endGame,play,other}=useContext(GameContext);
+    const router=useRouter();
   return (
     <button className={`${styles.back} pointer`} onClick={()=>{
       if(play) other.play();
-      endGame();
+      if(home) router.replace("/");
+      else endGame();
       }}>
         <FontAwesomeIcon  icon={faAngleLeft}/>
     </button>
--- components/number-riddle/Levels.jsx
+++ components/number-riddle/Levels.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from "./Levels.module.scss"
 import { useContext } from 'react'
 import GameContext from "../../store/number-riddle/GameContext";
+import BackButton from './BackButton';
 const Levels = () => {
     const {startGame,other,play}=useContext(GameContext);
     if(typeof window !== 'undefined'){
@@ -18,13 +19,21 @@ const Levels = () => {
     }
     
   return (
-    <ul className={styles.levels}>
+    <div className={styles.levels}>
+      <div>
+        <BackButton home={true} />
+        <h1>Select Size of puzzle</h1>
+        <span/>
+      </div>
+        <ul>
         {[3,4,5,6,7,8].map(each=><li key={each} onClick={()=> {
           if(play) other.play();
           startGame(each);
           openFullscreen();
         }}>{each}x{each}</li>)}
     </ul>
+    </div>
+    
   )
 }
 
