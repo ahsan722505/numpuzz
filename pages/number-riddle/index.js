753affdcd14a357d3a7c1bf08845d684e73800a7
ahsan722505 <ahsantahir722505@gmail.com>
2022-09-04 00:17:49 +0500

    deploying

--- pages/number-riddle/index.js
+++ pages/number-riddle/index.js
@@ -1,32 +1,38 @@
 import Head from "next/head";
 import { useDispatch, useSelector } from "react-redux";
 import App from "../../components/number-riddle/App";
-import {GameContextProvider} from "../../store/number-riddle/GameContext";
+import { GameContextProvider } from "../../store/number-riddle/GameContext";
 import { getBest } from "../../reduxStore/NumberRiddleSlice";
 import { useEffect } from "react";
-import { CircularProgress } from '@mui/material';
-const index = () => {
-  const {global :{loading:authLoading},numberRiddle : {loading,best}}=useSelector(state=> state);
-  const dispatch=useDispatch();
-  useEffect(()=>{
-    if(!best) dispatch(getBest());
-  },[]);
+import { CircularProgress } from "@mui/material";
+const Index = () => {
+  const {
+    global: { loading: authLoading },
+    numberRiddle: { loading, best },
+  } = useSelector((state) => state);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (!best) dispatch(getBest());
+  }, []);
 
   return (
     <>
       <Head>
         <title>Number Riddle</title>
-        <meta name="description" content="Numpuz: Number Riddle is a classic math puzzle game online available for free. Tap and move the wood number tiles, enjoy the magic of digit, coordinate your eyes, hands and brain." />
+        <meta
+          name="description"
+          content="Numpuz: Number Riddle is a classic math puzzle game online available for free. Tap and move the wood number tiles, enjoy the magic of digit, coordinate your eyes, hands and brain."
+        />
       </Head>
       <style global jsx>{`
         body {
-          background: #F9F9C5;
+          background: #f9f9c5;
         }
       `}</style>
-      { loading || authLoading && <CircularProgress/>}
-        {!loading && !authLoading && <App/>}
+      {loading || (authLoading && <CircularProgress />)}
+      {!loading && !authLoading && <App />}
     </>
-  )
-}
+  );
+};
 
-export default index
+export default Index;
