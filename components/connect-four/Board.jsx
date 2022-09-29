7684a42be7d8ab61334a1c5797054a1b996b8423
ahsan722505 <ahsantahir722505@gmail.com>
2022-09-29 19:43:11 +0500

    thinking

--- components/connect-four/Board.jsx
+++ components/connect-four/Board.jsx
@@ -4,7 +4,6 @@ import { Util } from "../../helpers/connect-four/util";
 import { useState } from "react";
 const Board = () => {
   const gameDim = 6;
-  const spacing = 2;
   const [board, setBoard] = useState(() => Util.CreateBoard(gameDim));
 
   return (
@@ -14,23 +13,20 @@ const Board = () => {
           <div
             key={rowInd}
             style={{
-              height: `${100 / gameDim - spacing - spacing / gameDim}%`,
-              marginBottom: `${spacing}%`,
-              marginTop: rowInd === 0 && `${spacing}%`,
+              height: `${100 / gameDim}%`,
             }}
           >
             {eachRow.map((_, cellInd) => {
               return (
                 <div
                   style={{
-                    width: `${100 / gameDim - spacing - spacing / gameDim}%`,
-                    marginRight: `${spacing}%`,
-                    marginLeft: cellInd === 0 && `${spacing}%`,
-                    backgroundColor: cellInd === 2 && "black",
+                    width: `${100 / gameDim}%`,
                   }}
                   key={cellInd}
                   className={styles.cell}
-                ></div>
+                >
+                  <div className={styles.circle} />
+                </div>
               );
             })}
           </div>
