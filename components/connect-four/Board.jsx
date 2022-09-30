6bae97f8ee2eac10bb76a15e751287914842bf9d
ahsan722505 <ahsantahir722505@gmail.com>
2022-09-30 16:43:00 +0500

    movement

--- components/connect-four/Board.jsx
+++ components/connect-four/Board.jsx
@@ -2,9 +2,30 @@ import React from "react";
 import styles from "./Board.module.scss";
 import { Util } from "../../helpers/connect-four/util";
 import { useState } from "react";
+import { useRef } from "react";
 const Board = () => {
   const gameDim = 6;
   const [board, setBoard] = useState(() => Util.CreateBoard(gameDim));
+  const boardRef = useRef(board);
+  const dropDisk = (col) => {
+    let i = 0;
+    const id = setInterval(() => {
+      console.log(boardRef.current);
+      if (
+        (boardRef.current[i] && boardRef.current[i][col] === 1) ||
+        i === board.length
+      ) {
+        clearInterval(id);
+        return;
+      }
+      const newBoard = Util.createCopy(boardRef.current);
+      newBoard[i][col] = 1;
+      if (newBoard[i - 1]) newBoard[i - 1][col] = 0;
+      boardRef.current = newBoard;
+      setBoard(newBoard);
+      i++;
+    }, 50);
+  };
 
   return (
     <div className={styles.board} onClick={(e) => console.log(e)}>
@@ -16,7 +37,7 @@ const Board = () => {
               height: `${100 / gameDim}%`,
             }}
           >
-            {eachRow.map((_, cellInd) => {
+            {eachRow.map((eachCell, cellInd) => {
               return (
                 <div
                   style={{
@@ -24,8 +45,12 @@ const Board = () => {
                   }}
                   key={cellInd}
                   className={styles.cell}
+                  onClick={() => dropDisk(cellInd)}
                 >
-                  <div className={styles.circle} />
+                  <div
+                    className={styles.circle}
+                    style={{ backgroundColor: eachCell === 1 && "black" }}
+                  />
                 </div>
               );
             })}
--- helpers/connect-four/util.js
+++ helpers/connect-four/util.js
@@ -4,9 +4,12 @@ export class Util {
     let currentRow = [];
     for (let i = 0; i < gameDim; i++) {
       currentRow = [];
-      for (let j = 0; j < gameDim; j++) currentRow.push(1);
+      for (let j = 0; j < gameDim; j++) currentRow.push(0);
       board.push(currentRow);
     }
     return board;
+  }
+  static createCopy(board) {
+    return board.map((each) => each.slice());
   }
 }
